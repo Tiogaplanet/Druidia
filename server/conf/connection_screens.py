06f4d2c330d9bc8bbc261d572d35eb8b2505c41a
Tiogaplanet <samuel.trassare@gmail.com>
2022-03-30 06:14:18 -1000

    Updated the login screen again.

--- server/conf/connection_screens.py
+++ server/conf/connection_screens.py
@@ -22,45 +22,51 @@ of the screen is done by the unlogged-in "look" command.
 
 from django.conf import settings
 from evennia import utils
-
-CONNECTION_SCREENA = """
+"""
+CONNECTION_SCREENA = \"""
 |b================================================================================|n
-
-
-  |g________                  .__     .___.__         
-  \______ \  _______  __ __ ||__|  __| _/||__||_____   
-   |    |  \ \_  __ \|  |  \|  | / __ | |  |\__  \  
-   |    `   \ |  | \/|  |  /|  ||/ /_/ | |  | / __ \_
-  /_______  / ||__|   ||____/ ||__||\____ | ||__|(____  /
-          \/                         \/          \/|n
-  
-
-  |yLet's set a course for Druidia.  -Lone Starr|n
-
-
-
+|=mdrop grenade|n         |=mnw|n                |=mn|n                 |=mne|n          |=mshift panel|n
+|=mlanguages|n                                                              |=minventory|n
+|=mwhisper|n                                                                  |=mccreate|n
+|=msay hi|n                                                                    |=maccess|n
+|=mstyle|n                                                                      |=mcboot|n
+|=mlook|n           |g________                  .__     .___.__                    |=mpage|n
+|=mwho|n            |g\______ \  _______  __ __ ||__|  __| _/||__||_____               |=ml e|n
+|=mnw|n              |g|    |  \ \_  __ \|  |  \|  | / __ | |  |\__  \               |=mne|n
+|=mw|n               |g|   _|   \ |  | \/|  |  /|  ||/ /_/ | |  | / __ \_              |=me|n
+|=mw|n              |g/_______  / ||__|   ||____/ ||__||\____ | ||__|(____  /              |=me|n
+|=msw|n                     |g\/                         \/          \/|n              |=mse|n
+|=mdig|n                                                                          |=mooc|n
+|=mhelp|n                                                                        |=mtime|n
+|=moption|n           |yLet's set a course for Druidia.  -Lone Starr|n              |=mcemit|n
+|=mccreate|n                                                                   |=msetdes|n
+|=mshutdown|n                                                                |=mchannels|n
+|=mlook west|n            |=msw|n                |=ms|n                 |=mse|n          |=mpush button|n
 
      Connect to your account                          Create an account
   |wconnect <username> <password>|n                 |wcreate <username> <password>|n
            Enter |whelp|n for more info | Enter |wlook|n to show this screen
 |b================================================================================|n"""
-
-CONNECTION_SCREENB = """
+"""
+CONNECTION_SCREENB = \"""
 |b================================================================================|n
-
-
-  |r________                  .__     .___.__         
-  \______ \  _______  __ __ ||__|  __| _/||__||_____   
-   |    |  \ \_  __ \|  |  \|  | / __ | |  |\__  \  
-   |    `   \ |  | \/|  |  /|  ||/ /_/ | |  | / __ \_
-  /_______  / ||__|   ||____/ ||__||\____ | ||__|(____  /
-          \/                         \/          \/|n
-
-  
-  |yNever underestimate the power of the Schwartz!  -Yogurt|n
-
-
-
+|=mdrop grenade|n         |=mne|n                |=mn|n                 |=mnw|n          |=mshift panel|n
+|=mlanguages|n                                                              |=minventory|n
+|=mwhisper|n                                                                  |=mccreate|n
+|=msay hi|n                                                                    |=maccess|n
+|=mstyle|n                                                                      |=mcboot|n
+|=mlook|n           |r________                  .__     .___.__                    |=mpage|n
+|=mwho|n            |r\______ \  _______  __ __ ||__|  __| _/||__||_____               |=ml w|n
+|=mne|n              |r|    |  \ \_  __ \|  |  \|  | / __ | |  |\__  \               |=mnw|n
+|=me|n               |r|   _|   \ |  | \/|  |  /|  ||/ /_/ | |  | / __ \_              |=mw|n
+|=me|n              |r/_______  / ||__|   ||____/ ||__||\____ | ||__|(____  /              |=mw|n
+|=mse|n                     |r\/                         \/          \/|n              |=msw|n
+|=mdig|n                                                                          |=mooc|n
+|=mhelp|n                                                                        |=mtime|n
+|=moption|n     |yGo then.  There are other worlds than these. -Jake Chambers|n     |=mcemit|n
+|=mccreate|n                                                                   |=msetdes|n
+|=mshutdown|n                                                                |=mchannels|n
+|=mlook west|n            |=mse|n                |=ms|n                 |=msw|n          |=mpush button|n
 
      Connect to your account                          Create an account
   |wconnect <username> <password>|n                 |wcreate <username> <password>|n
@@ -69,40 +75,44 @@ CONNECTION_SCREENB = """
 
 CONNECTION_SCREENC = """
 |b================================================================================|n
-
-
-  |c________                  .__     .___.__         
-  \______ \  _______  __ __ ||__|  __| _/||__||_____   
-   |    |  \ \_  __ \|  |  \|  | / __ | |  |\__  \  
-   |    `   \ |  | \/|  |  /|  ||/ /_/ | |  | / __ \_
-  /_______  / ||__|   ||____/ ||__||\____ | ||__|(____  /
-          \/                         \/          \/|n
-
-  
-  |yWhen does this happen in the movie?  -Dark Helmet|n
-
-
-
-
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//
+|r////////////////////////////////////////////////////////////////////////////////|n
+|y////////////////////////////////////////////////////////////////////////////////|n
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B/|c________|B/|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y/|c.__|B/|b/|B//|y/|c.___.__|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B/|c\______ \  _______  __ __ ||__|  __| _/||__||_____   
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B/ |c|    |  \ \_  __ \|  |  \|  | / __ | |  |\__  \  
+|r/////////////// |c|   _|   \ |  | \/|  |  /|  ||/ /_/ | |  | / __ \_
+|y///////////////|c/_______  / ||__|   ||____/ ||__||\____ | ||__|(____  /
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B/|c\/                         \/          \/|n
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//
+|r////////////////|yWhen does this happen in the movie?  -Dark Helmet|n
+|y////////////////////////////////////////////////////////////////////////////////|n
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//
+|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|y//|r//|B/|b/|B//|n
      Connect to your account                          Create an account
   |wconnect <username> <password>|n                 |wcreate <username> <password>|n
            Enter |whelp|n for more info | Enter |wlook|n to show this screen
 |b================================================================================|n"""
-
-CONNECTION_SCREEND = """
+"""
+CONNECTION_SCREEND = \"""
 |b================================================================================|n
 
 
-  |m________                  .__     .___.__         
-  \______ \  _______  __ __ ||__|  __| _/||__||_____   
-   |    |  \ \_  __ \|  |  \|  | / __ | |  |\__  \  
-   |    `   \ |  | \/|  |  /|  ||/ /_/ | |  | / __ \_
-  /_______  / ||__|   ||____/ ||__||\____ | ||__|(____  /
-          \/                         \/          \/|n
+               |m________                  .__     .___.__         
+               \______ \  _______  __ __ ||__|  __| _/||__||_____   
+                |    |  \ \_  __ \|  |  \|  | / __ | |  |\__  \  
+                |   _|   \ |  | \/|  |  /|  ||/ /_/ | |  | / __ \_
+               /_______  / ||__|   ||____/ ||__||\____ | ||__|(____  /
+                       \/                         \/          \/|n
 
   
-  |yWe can't stop, it's too dangerous! We have to slow down first!
-                                                         -Colonel Sandurz|n
+           |yNever underestimate the power of the Schwartz!  -Yogurt|n
+  
+
 
 
 
